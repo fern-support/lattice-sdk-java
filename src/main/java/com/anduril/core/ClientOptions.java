74fdfa961976f8364e996b316b07dc525f28eb61
Fern Support <126544928+fern-support@users.noreply.github.com>
2025-12-15 18:08:55 -0500

    SDK regeneration

--- src/main/java/com/anduril/core/ClientOptions.java
+++ src/main/java/com/anduril/core/ClientOptions.java
@@ -35,10 +35,9 @@ public final class ClientOptions {
         this.headers.putAll(headers);
         this.headers.putAll(new HashMap<String, String>() {
             {
-                put("User-Agent", "com.anduril:lattice-sdk/5.0.0");
                 put("X-Fern-Language", "JAVA");
                 put("X-Fern-SDK-Name", "com.anduril.fern:api-sdk");
-                put("X-Fern-SDK-Version", "5.0.0");
+                put("X-Fern-SDK-Version", "0.0.4494");
             }
         });
         this.headerSuppliers = headerSuppliers;
